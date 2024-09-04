9421eab602be8468d13b68a60bad4636bfca7e31
jakelanghel <jakelangheldev@gmail.com>
2024-09-03 21:42:51 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from "./shared/Global";
+import { images } from "./constant/images";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      Jake
+    </>
+  );
+}
+
+export default App;
---
+++ src/constant/images.js
@@ -0,0 +1,92 @@
+// images.js
+
+// Importing all images from ../assets/
+import iconAddToCart from "../assets/icon-add-to-cart.svg";
+import iconCarbonNeutral from "../assets/icon-carbon-neutral.svg";
+import iconDecrementQuantity from "../assets/icon-decrement-quantity.svg";
+import iconIncrementQuantity from "../assets/icon-increment-quantity.svg";
+import iconOrderConfirmed from "../assets/icon-order-confirmed.svg";
+import iconRemoveItem from "../assets/icon-remove-item.svg";
+import illustrationEmptyCart from "../assets/illustration-empty-cart.svg";
+import imageBaklavaDesktop from "../assets/image-baklava-desktop.jpg";
+import imageBaklavaMobile from "../assets/image-baklava-mobile.jpg";
+import imageBaklavaTablet from "../assets/image-baklava-tablet.jpg";
+import imageBaklavaThumbnail from "../assets/image-baklava-thumbnail.jpg";
+import imageBrownieDesktop from "../assets/image-brownie-desktop.jpg";
+import imageBrownieMobile from "../assets/image-brownie-mobile.jpg";
+import imageBrownieTablet from "../assets/image-brownie-tablet.jpg";
+import imageBrownieThumbnail from "../assets/image-brownie-thumbnail.jpg";
+import imageCakeDesktop from "../assets/image-cake-desktop.jpg";
+import imageCakeMobile from "../assets/image-cake-mobile.jpg";
+import imageCakeTablet from "../assets/image-cake-tablet.jpg";
+import imageCakeThumbnail from "../assets/image-cake-thumbnail.jpg";
+import imageCremeBruleeDesktop from "../assets/image-creme-brulee-desktop.jpg";
+import imageCremeBruleeMobile from "../assets/image-creme-brulee-mobile.jpg";
+import imageCremeBruleeTablet from "../assets/image-creme-brulee-tablet.jpg";
+import imageCremeBruleeThumbnail from "../assets/image-creme-brulee-thumbnail.jpg";
+import imageMacaronDesktop from "../assets/image-macaron-desktop.jpg";
+import imageMacaronMobile from "../assets/image-macaron-mobile.jpg";
+import imageMacaronTablet from "../assets/image-macaron-tablet.jpg";
+import imageMacaronThumbnail from "../assets/image-macaron-thumbnail.jpg";
+import imageMeringueDesktop from "../assets/image-meringue-desktop.jpg";
+import imageMeringueMobile from "../assets/image-meringue-mobile.jpg";
+import imageMeringueTablet from "../assets/image-meringue-tablet.jpg";
+import imageMeringueThumbnail from "../assets/image-meringue-thumbnail.jpg";
+import imagePannaCottaDesktop from "../assets/image-panna-cotta-desktop.jpg";
+import imagePannaCottaMobile from "../assets/image-panna-cotta-mobile.jpg";
+import imagePannaCottaTablet from "../assets/image-panna-cotta-tablet.jpg";
+import imagePannaCottaThumbnail from "../assets/image-panna-cotta-thumbnail.jpg";
+import imageTiramisuDesktop from "../assets/image-tiramisu-desktop.jpg";
+import imageTiramisuMobile from "../assets/image-tiramisu-mobile.jpg";
+import imageTiramisuTablet from "../assets/image-tiramisu-tablet.jpg";
+import imageTiramisuThumbnail from "../assets/image-tiramisu-thumbnail.jpg";
+import imageWaffleDesktop from "../assets/image-waffle-desktop.jpg";
+import imageWaffleMobile from "../assets/image-waffle-mobile.jpg";
+import imageWaffleTablet from "../assets/image-waffle-tablet.jpg";
+import imageWaffleThumbnail from "../assets/image-waffle-thumbnail.jpg";
+
+export const images = {
+  iconAddToCart,
+  iconCarbonNeutral,
+  iconDecrementQuantity,
+  iconIncrementQuantity,
+  iconOrderConfirmed,
+  iconRemoveItem,
+  illustrationEmptyCart,
+  imageBaklavaDesktop,
+  imageBaklavaMobile,
+  imageBaklavaTablet,
+  imageBaklavaThumbnail,
+  imageBrownieDesktop,
+  imageBrownieMobile,
+  imageBrownieTablet,
+  imageBrownieThumbnail,
+  imageCakeDesktop,
+  imageCakeMobile,
+  imageCakeTablet,
+  imageCakeThumbnail,
+  imageCremeBruleeDesktop,
+  imageCremeBruleeMobile,
+  imageCremeBruleeTablet,
+  imageCremeBruleeThumbnail,
+  imageMacaronDesktop,
+  imageMacaronMobile,
+  imageMacaronTablet,
+  imageMacaronThumbnail,
+  imageMeringueDesktop,
+  imageMeringueMobile,
+  imageMeringueTablet,
+  imageMeringueThumbnail,
+  imagePannaCottaDesktop,
+  imagePannaCottaMobile,
+  imagePannaCottaTablet,
+  imagePannaCottaThumbnail,
+  imageTiramisuDesktop,
+  imageTiramisuMobile,
+  imageTiramisuTablet,
+  imageTiramisuThumbnail,
+  imageWaffleDesktop,
+  imageWaffleMobile,
+  imageWaffleTablet,
+  imageWaffleThumbnail,
+};
